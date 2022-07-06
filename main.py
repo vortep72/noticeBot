d27462025655e80a45801361d30d5a3f0e2967c1
Aleksandr Petrov <aleksandr.petrov@x5.ru>
2022-07-06 19:07:45 +0300

    add requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import telebot
 import schedule
 from security import TOKEN, tg_id
-import datetime
+
+
 bot = telebot.TeleBot(TOKEN)
+
+# Ежедневные уведомления
 
 
 def my_daily_task1(tg_id):
@@ -19,18 +22,14 @@ def my_daily_task3(tg_id):
 
 def my_daily_task4(tg_id):
     bot.send_message(tg_id, "парни, кажется вам нужен отдых, попейте чаю или покурите")
-    
-
-def my_daily_task5(tg_id):
-    bot.send_message(tg_id, "Хэй! У меня новый дом и я тут одна!")
-if schedule.every().wednesday.at("18:58").do(my_daily_task5, tg_id):
-    print('Cообщение для my_daily_task5 отправлено!')
 
 
 schedule.every().day.at("08:45").do(my_daily_task1, tg_id)
 schedule.every().day.at("15:45").do(my_daily_task2, tg_id)
 schedule.every().day.at("14:30").do(my_daily_task3, tg_id)
 schedule.every().day.at("13:00").do(my_daily_task4, tg_id)
+
+# Еженедельные уведомления
 
 
 def weekly_task1(tg_id):
